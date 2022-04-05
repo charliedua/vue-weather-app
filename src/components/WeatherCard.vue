c205793dafe5f687ea5a227e724f00764e45d19e
charliedua <duaanmol2@gmail.com>
2022-04-05 12:56:17 +1000

    added cards

---
+++ src/components/WeatherCard.vue
@@ -0,0 +1,72 @@
+<script>
+export default {
+  name: "WeatherCard",
+  props: {
+    objData: Object
+    // lowTemp: Number,
+    // highTemp: Number,
+    // location: Object,
+    // weatherIcon: String,
+    // tempUnit: String,
+  },
+  // data() {
+  //   return {
+  //     lowTemp: 27.5,
+  //     highTemp: 55.3,
+  //     location: {
+  //       city: "Melbourne",
+  //       state: "VIC",
+  //     },
+  //     weatherIcon:
+  //       "https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png",
+  //     tempUnit: "°C",
+  //   };
+  // },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Weather</h1>
+    <img :src="objData.weatherIcon" />
+    <ul>
+      <li>
+        Low : <span id="high">{{ objData.lowTemp }} {{ objData.tempUnit }}</span>
+      </li>
+      <li>
+        High : <span id="low">{{ objData.highTemp }} {{ objData.tempUnit }}</span>
+      </li>
+    </ul>
+    <p>
+      Showing temps of : <b>{{ objData.location.city }} {{ objData.location.state }}</b>
+    </p>
+  </div>
+</template>
+
+<style scoped>
+div {
+  border: 3px black solid;
+  padding: 10px;
+  width: 400px;
+  font-size: 2em;
+  font-family: "Comic Sans MS", "Comic Sans", cursive;
+  text-align: center;
+  box-shadow: 5px 5px #888888;
+}
+
+img {
+  margin: auto;
+  display: block;
+  width: 8em;
+}
+li {
+  list-style-type: none;
+  margin-left: -15%;
+}
+#high {
+  color: red;
+}
+#low {
+  color: blue;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,70 @@
 <script>
-import HomeHelper from '../components/HomeHelper.vue';
-
-
+import WeatherCard from "../components/WeatherCard.vue";
 
 export default {
   components: {
-    HomeHelper,
+    WeatherCard,
+  },
+  data() {
+    return {
+      tempData: [
+        {
+          lowTemp: 27.5,
+          highTemp: 55.3,
+          location: {
+            city: "Melbourne",
+            state: "VIC",
+          },
+          weatherIcon:
+            "https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png",
+          tempUnit: "°C",
+        },
+        {
+          lowTemp: 10.5,
+          highTemp: 17.6,
+          location: {
+            city: "Melbourne",
+            state: "VIC",
+          },
+          weatherIcon:
+            "https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png",
+          tempUnit: "°C",
+        },
+        {
+          lowTemp: -10.6,
+          highTemp: 55.3,
+          location: {
+            city: "Amaroo",
+            state: "ACT",
+          },
+          weatherIcon:
+            "https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png",
+          tempUnit: "°C",
+        },
+      ],
+    };
   },
 };
 </script>
 
 <template>
-  <div>
-    <home-helper msg="HELLO" />
+  <div class="main">
+    <div class="card" v-for="weather in tempData" :key="weather.lowTemp">
+      <weather-card :objData="weather" />
+    </div>
   </div>
 </template>
+
+<style scoped>
+.main {
+  display: flex;
+  justify-content: center;
+}
+
+.card {
+    margin-right: 16px;
+}
+.card:last-child{
+    margin-right: 0px;
+}
+</style>
